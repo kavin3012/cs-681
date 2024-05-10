9f194949e3fd1b6bdc5d805ba3d52e5332ea98e5
kavin3012 <113407225+kavin3012@users.noreply.github.com>
2024-05-10 01:56:41 -0400

    Delete hw11 directory

--- hw11/src/edu/umb/cs681/hw11/fs/FSVisitor.java
+++
@@ -1,9 +0,0 @@
-package edu.umb.cs681.hw11.fs;
-
-public interface FSVisitor {
-
-    public abstract void visit(Link link);
-    public abstract void visit(Directory dir);
-    public abstract void visit(File file);
-    
-}
