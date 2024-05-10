c7f63e49b59bd9d67bb6d7accc3f885a0b7ec95e
kavin3012 <113407225+kavin3012@users.noreply.github.com>
2024-05-10 01:54:42 -0400

    Delete hw07 directory

--- hw07/src/edu/umb/cs681/hw07/fs/FSVisitor.java
+++
@@ -1,9 +0,0 @@
-package edu.umb.cs681.hw07.fs;
-
-public interface FSVisitor {
-
-    public abstract void visit(Link link);
-    public abstract void visit(Directory dir);
-    public abstract void visit(File file);
-    
-}
